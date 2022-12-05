0184b6bb3f7f476de56984dc2c09e5658e0882d8
Ryuji Hirano <seilegs777@gmail.com>
2022-12-05 23:29:40 +0900

    change

--- test.bash
+++ test.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Ryuji Hirano
 # SPDX-License-Identifier: BSD-3-Clause
-　
+
 ng () {
       echo NG at Line $1  
       res=1
@@ -13,9 +13,8 @@ res=0
 out=$(seq 5 | ./plus_stdin)
 [ "${out}" = 15 ] || ng ${LINENO}
 
-
-out=$(st2[2:] | ./plus_time)
-[ "${out}" = 正解 ] || ng ${LINENO}
+out=$(echo st2[2:] | ./plus_time)
+[ "${out}" != "正解" ] || ng ${LINENO}
 
 
 [ "$res" = 0 ] && echo OK
